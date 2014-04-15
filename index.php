03d7c154e4d59506d49283f9ea80f9ea624a5052
alternat0r <alternator99@gmail.com>
2014-04-15 21:20:46 +0800

    put commend on .bat file; User need to change the bat file conf; Put comment on index.php;

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-	//error_reporting(0);
-	//echo "<META HTTP-EQUIV=\"refresh\" CONTENT=\"900\">";
 	date_default_timezone_set('Asia/Kuala_Lumpur'); 
 	
+	// check new updates on every $cycle_time time 
 	$cycle_time = "7"; // in minutes
 	
 	for ($x=1; $x<5000; $x++)
@@ -21,7 +20,4 @@
 		}
 		echo "\n";
 	}	
-	/*$page = $_SERVER['PHP_SELF'];
-	$sec = "5";
-	header("Refresh: $sec; url=$page");*/
 ?>
--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 echo off
 cls
-title Bot @OnlyMalayNews
+title Bot @Your_Twitter_Name
+
+:: change the php path location based on your environment
 C:\wamp\bin\php\php5.4.12\php.exe c:\wamp\www\beburung\index.php
